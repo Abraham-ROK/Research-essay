dc3188efa580bf2fcc40871f33cba00c9848462f
Abraham-ROK <abraham.kenneth@outlook.fr>
2023-05-02 03:22:10 +0200

    feature: App.js

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [memberData, setmemberData] = useState([])
 
-export default App;
+  useEffect (() => {
+    fetch('/members', {
+      'methods' : 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer`,
+      }
+    })
+    .then(res => res.json())
+    .then(memberData => {
+      setmemberData(memberData)
+      console.log(memberData)
+      }
+    )
+  },[])
+
+  return (
+  <div>
+    {(typeof memberData.members === 'undefined') ? (
+      <p>loading...</p>
+      ) : (
+        memberData.members.map((member, i) => (
+        <p key={i}>{member}</p>
+        ))
+      )}
+  </div>
+  )
+}
+export default App
