6570fc9064212e668dba48da92b20db0fd0476f7
Abraham-ROK <abraham.kenneth@outlook.fr>
2023-06-19 01:10:34 +0200

    feature: Jenkinsfile

--- web-app/backend/server.py
+++ web-app/backend/server.py
@@ -3,6 +3,7 @@ from controllers import app
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=5001)
+    # app.run(debug=True,host="localhost", port=5001)
 
 
 
@@ -16,5 +17,5 @@ if __name__ == "__main__":
 
 
 # if __name__ == "__main__":
-#     # app.run(debug=True,host="localhost", port=5001)
+    # app.run(debug=True,host="localhost", port=5001)
 #     app.run(debug=True,host='0.0.0.0', port=5001)
