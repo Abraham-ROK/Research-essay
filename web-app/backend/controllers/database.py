1b7db37c6e294d681ae783cbe4a6f3533416210b
Abraham-ROK <abraham.kenneth@outlook.fr>
2023-07-18 04:03:19 +0200

    feature: loctust

--- web-app/backend/controllers/database.py
+++ web-app/backend/controllers/database.py
@@ -3,8 +3,8 @@ import pymongo
 
 
 def get_db():
-    mongo_db_client = pymongo.MongoClient("mongodb://mongo_user:mongo_user_password@Mongo_Database:27017/")
-    # mongo_db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    # mongo_db_client = pymongo.MongoClient("mongodb://mongo_user:mongo_user_password@Mongo_Database:27017/")
+    mongo_db_client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = mongo_db_client["food_db"]
     
     return db
--- web-app/backend/locust.py
+++ web-app/backend/locust.py
@@ -1,4 +1,8 @@
 import time
+from faker import Faker
+import random
+import string
+
 
 from locust import HttpUser, TaskSet, task, between
 
@@ -6,6 +10,8 @@ from locust import HttpUser, TaskSet, task, between
 #     @task(1)
 #     pass
 
+
+fake = Faker()
 class WebsiteUser(HttpUser):
     # tasks = [UserBehavior]
     wait_time = between(1, 5)
@@ -14,9 +20,15 @@ class WebsiteUser(HttpUser):
     @task
     def food_page(self) :
         self.client.get(url="http://localhost:5001/foods") 
+        self.client.post("http://localhost:5001/foods", json={  "Name": fake.word(),
+                                                                "Proteins": random.randint(5, 50),
+                                                                "Carbs": random.randint(5, 50),
+                                                                "Fats": random.randint(5, 50)})
 
     # @task
     # def slow_page(self):
     #     self.client.get(url="/slow") locust -f locust.py --host http://localhost:5001/foods --users 5000 -- spawn-rate 50
     
+
+
     # locust -f locust.py --host=http://localhost:5001/foods --users=50 --spawn-rate=5 --run-time=1m --headless
